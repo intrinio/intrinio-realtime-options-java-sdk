98fc12316b01fb32a9e3596423308e07b6d3e577
ssnyder <ssnyder@intrinio.com>
2022-10-25 01:46:11 -0500

    Trade is done

--- src/intrinio/Trade.java
+++ src/intrinio/Trade.java
@@ -6,7 +6,7 @@ import java.nio.charset.StandardCharsets;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 
-public record Trade(String symbol, double price, long size, long totalVolume, double timestamp) {
+public record Trade(String symbol, double price, long size, double timestamp, long totalVolume, double askPriceAtExecution, double bidPriceAtExecution, double underlyingPriceAtExecution) {
 
 	public float getStrikePrice() {
 		return Float.parseFloat(this.symbol.substring(this.symbol.indexOf('_') + 8));
@@ -30,15 +30,15 @@ public record Trade(String symbol, double price, long size, long totalVolume, do
 	public String getUnderlyingSymbol() { return this.symbol.substring(0, this.symbol.indexOf('_')).trim(); }
 	
 	public String toString() {
-		String s =
-				"Trade (" +
-				"Symbol: " + this.symbol +
-				", Price: " + this.price +
-				", Size: " + this.size +
-				", Total Volume: " + this.totalVolume +
-				", Timestamp: " + this.timestamp +
-				")";
-		return s;
+		return String.format("Quote (Symbol: %s, Price: %s, Size: %s, Timestamp: %s, TotalVolume: %s, AskPriceAtExecution: %s, BidPriceAtExecution: %s, UnderlyingPriceAtExecution: %s)",
+				this.symbol,
+				this.price,
+				this.size,
+				this.timestamp,
+				this.totalVolume,
+				this.askPriceAtExecution,
+				this.bidPriceAtExecution,
+				this.underlyingPriceAtExecution);
 	}
 
 	static int getMessageSize(){
@@ -46,47 +46,104 @@ public record Trade(String symbol, double price, long size, long totalVolume, do
 	}
 	
 	public static Trade parse(byte[] bytes) {
-		String symbol = StandardCharsets.US_ASCII.decode(ByteBuffer.wrap(bytes, 0, 21)).toString();
+		//byte structure:
+		// symbol [0-19]
+		// event type [20]
+		// price [21-24]
+		// price type [25]
+		// underlying price type [26]
+		// size [27-30]
+		// timestamp [31-38]
+		// total volume [39-46]
+		// ask price at execution [47-50]
+		// bid price at execution [51-54]
+		// underlying price at execution [55-58]
+
+		String symbol = StandardCharsets.US_ASCII.decode(ByteBuffer.wrap(bytes, 0, 20)).toString();
 		
-		ByteBuffer priceBuffer = ByteBuffer.wrap(bytes, 22, 8);
+		ByteBuffer priceBuffer = ByteBuffer.wrap(bytes, 21, 4);
 		priceBuffer.order(ByteOrder.LITTLE_ENDIAN);
-		double price = priceBuffer.getDouble();
+		int unscaledPrice = priceBuffer.getInt();
+
+		PriceType scaler = PriceType.fromInt(bytes[25]);
+		PriceType underlyingScaler = PriceType.fromInt(bytes[26]);
+		double price = scaler.getScaledValue(unscaledPrice);
 		
-		ByteBuffer sizeBuffer = ByteBuffer.wrap(bytes, 30, 4);
+		ByteBuffer sizeBuffer = ByteBuffer.wrap(bytes, 27, 4);
 		sizeBuffer.order(ByteOrder.LITTLE_ENDIAN);
 		long size = Integer.toUnsignedLong(sizeBuffer.getInt());
 		
-		ByteBuffer timeStampBuffer = ByteBuffer.wrap(bytes, 34, 8);
+		ByteBuffer timeStampBuffer = ByteBuffer.wrap(bytes, 31, 8);
 		timeStampBuffer.order(ByteOrder.LITTLE_ENDIAN);
-		double timestamp = timeStampBuffer.getDouble();
+		double timestamp = ((double) timeStampBuffer.getLong()) / 1_000_000_000.0D;
 		
-		ByteBuffer volumeBuffer = ByteBuffer.wrap(bytes, 42, 8);
+		ByteBuffer volumeBuffer = ByteBuffer.wrap(bytes, 39, 8);
 		volumeBuffer.order(ByteOrder.LITTLE_ENDIAN);
 		long totalVolume = volumeBuffer.getLong();
+
+		ByteBuffer askPriceAtExecutionBuffer = ByteBuffer.wrap(bytes, 47, 4);
+		askPriceAtExecutionBuffer.order(ByteOrder.LITTLE_ENDIAN);
+		double askPriceAtExecution = scaler.getScaledValue(askPriceAtExecutionBuffer.getInt());
+
+		ByteBuffer bidPriceAtExecutionBuffer = ByteBuffer.wrap(bytes, 51, 4);
+		bidPriceAtExecutionBuffer.order(ByteOrder.LITTLE_ENDIAN);
+		double bidPriceAtExecution = scaler.getScaledValue(bidPriceAtExecutionBuffer.getInt());
+
+		ByteBuffer underlyingPriceAtExecutionBuffer = ByteBuffer.wrap(bytes, 55, 4);
+		underlyingPriceAtExecutionBuffer.order(ByteOrder.LITTLE_ENDIAN);
+		double underlyingPriceAtExecution = underlyingScaler.getScaledValue(underlyingPriceAtExecutionBuffer.getInt());
 		
-		return new Trade(symbol, price, size, totalVolume, timestamp);
+		return new Trade(symbol, price, size, timestamp, totalVolume, askPriceAtExecution, bidPriceAtExecution, underlyingPriceAtExecution);
 	}
 	
 	public static Trade parse(ByteBuffer bytes) {
-		String symbol = StandardCharsets.US_ASCII.decode(bytes.slice(0, 21)).toString();
+		//byte structure:
+		// symbol [0-19]
+		// event type [20]
+		// price [21-24]
+		// price type [25]
+		// underlying price type [26]
+		// size [27-30]
+		// timestamp [31-38]
+		// total volume [39-46]
+		// ask price at execution [47-50]
+		// bid price at execution [51-54]
+		// underlying price at execution [55-58]
+		String symbol = StandardCharsets.US_ASCII.decode(bytes.slice(0, 20)).toString();
 		
-		ByteBuffer priceBuffer = bytes.slice(22, 8);
+		ByteBuffer priceBuffer = bytes.slice(21, 4);
 		priceBuffer.order(ByteOrder.LITTLE_ENDIAN);
-		double price = priceBuffer.getDouble();
+		int unscaledPrice = priceBuffer.getInt();
+
+		PriceType scaler = PriceType.fromInt(bytes.get(25));
+		PriceType underlyingScaler = PriceType.fromInt(bytes.get(26));
+		double price = scaler.getScaledValue(unscaledPrice);
 		
-		ByteBuffer sizeBuffer = bytes.slice(30, 4);
+		ByteBuffer sizeBuffer = bytes.slice(27, 4);
 		sizeBuffer.order(ByteOrder.LITTLE_ENDIAN);
 		long size = Integer.toUnsignedLong(sizeBuffer.getInt());
 		
-		ByteBuffer timeStampBuffer = bytes.slice(34, 8);
+		ByteBuffer timeStampBuffer = bytes.slice(31, 8);
 		timeStampBuffer.order(ByteOrder.LITTLE_ENDIAN);
-		double timestamp = timeStampBuffer.getDouble();
+		double timestamp = ((double) timeStampBuffer.getLong()) / 1_000_000_000.0D;
 		
-		ByteBuffer volumeBuffer = bytes.slice(42, 8);
+		ByteBuffer volumeBuffer = bytes.slice(39, 8);
 		volumeBuffer.order(ByteOrder.LITTLE_ENDIAN);
 		long totalVolume = volumeBuffer.getLong();
-		
-		return new Trade(symbol, price, size, totalVolume, timestamp);
+
+		ByteBuffer askPriceAtExecutionBuffer = bytes.slice(47, 4);
+		askPriceAtExecutionBuffer.order(ByteOrder.LITTLE_ENDIAN);
+		double askPriceAtExecution = scaler.getScaledValue(askPriceAtExecutionBuffer.getInt());
+
+		ByteBuffer bidPriceAtExecutionBuffer = bytes.slice(51, 4);
+		bidPriceAtExecutionBuffer.order(ByteOrder.LITTLE_ENDIAN);
+		double bidPriceAtExecution = scaler.getScaledValue(bidPriceAtExecutionBuffer.getInt());
+
+		ByteBuffer underyingPriceAtExecutionBuffer = bytes.slice(55, 4);
+		underyingPriceAtExecutionBuffer.order(ByteOrder.LITTLE_ENDIAN);
+		double underlyingPriceAtExecution = underlyingScaler.getScaledValue(underyingPriceAtExecutionBuffer.getInt());
+
+		return new Trade(symbol, price, size, timestamp, totalVolume, askPriceAtExecution, bidPriceAtExecution, underlyingPriceAtExecution);
 	}
 	
 }
